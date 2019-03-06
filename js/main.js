1bc2636913ea199e6e2dccc010defa9c329f4d70
cheskyposen <shragaposen@gmail.com>
2019-03-06 11:31:36 -0500

    Midpoint ajax quiz

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//Run jQuery after the document is fully loaded.
+/*//Run jQuery after the document is fully loaded.
 $(document).ready(
     //The function that does the stuff.
     function () {
@@ -23,4 +23,31 @@ $(document).ready(
             })
         })
     }
-)
+);*/
+
+$(document).ready(
+    $('button').click(
+        function () {
+            //Make the AJAX call
+            $.ajax('http://api.tvmaze.com/singlesearch/shows?q=' + $('input').val(), {
+                method: "GET",
+                dataType: "json"
+            })
+            //After the data comes back, use this function
+                .done(
+                    function (data) {
+                        //Add the name
+                        $('#name').html(data.name);
+                        //Add the episodes
+                       // data.forEach(function (episode) {
+                            $('#episodeList').html('<tr>'+
+                                '<td><a href="' + data.officialSite + '"><img src="' + data.image.medium + '"></a></td>' +
+                                '<td>' + data.rating.average + '</td>' +
+                                '<td>' + data.runtime + '</td>' +
+                                '<td>' + data.summary + '</td>' +
+                                +' </tr>')
+                        //})
+                    })
+        }
+    )
+);
